a3de22f78ba632632776d0fb8f87688ddf6f28a7
ccpink <54963606+ccpink@users.noreply.github.com>
2020-12-12 16:24:23 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -183,17 +183,17 @@ std::vector<std::string> getOpenSpacesZombie(Zombie zombie) {
     }
 
     if (canHeadNorth && canHeadWest) {
-        if (grid[xPos + 1][yPos + 1] == 1) //North-East
+        if (grid[xPos + 1][yPos + 1] == 1)
         {
-            openSpaces.emplace_back("North-West");
+            openSpaces.emplace_back("North-East");
             targeted = true;
         }
     }
 
     if (canHeadNorth && canHeadEast){
-        if (grid[xPos - 1][yPos + 1] == 1) //North-West
+        if (grid[xPos - 1][yPos + 1] == 1)
         {
-            openSpaces.emplace_back("North-East");
+            openSpaces.emplace_back("North-West");
             targeted = true;
         }
     }
